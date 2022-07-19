7452486031c49a0c0e43437948ebcfaaafd83822
Vaishnavi Desale <85835664+VaishnaviDesale@users.noreply.github.com>
2022-07-19 15:20:31 +0530

    Update urls.py

--- Series Recommender Chatbot/chatBotProject/chatBotProject/urls.py
+++ Series Recommender Chatbot/chatBotProject/chatBotProject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from django.urls import include, re_path
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	re_path(r'^', include('chatBot.urls'))
+	re_path(r'/', include('chatBot.urls'))
 ]
